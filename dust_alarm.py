711d8661fd5b8a946558e97e886d9fc828bda99a
Akil Marshall <akilmarshall@gmail.com>
2022-12-09 13:03:49 -1000

    wrote some comments

--- dust_alarm.py
+++ dust_alarm.py
@@ -1,6 +1,6 @@
 #!/local/python3/bin/python3
 """
-Version: Alpha
+Version: Beta
 
 This program checks at an interval of 5 minutes the particle count in Keck's inner dome against a threashold.
 When the particle count exceeds this threshold an audio alarm is played.
@@ -31,9 +31,14 @@ TOLERANCE = args.tolerance
 
 def sound_file(particles, most=500):
     """
-    most is equal to the audio file for the largest amount of particles audio warnings are issued for.
-    if new files are created update most accordingly
+    For a given particle count, return the path to the appropreate sound file.
+    i.e. for a particle count of 201 return the path to the sound file 200.wav
+    'the particle count has exceeded 200 particles per liter of air'
+
+    warning: if called with a particle count of less than 50, None is returned
     """
+    # most is equal to the audio file for the largest amount of particles audio warnings are issued for.
+    # if new files are created update most accordingly
     intervals = [(i, i + 50) for i in range(50, most, 50)]
     for (a, b) in intervals:
         if a < particles <= b:
@@ -42,6 +47,9 @@ def sound_file(particles, most=500):
         return SOUND_PATH / f'{most}.wav'
 
 def particles():
+    """
+    Call and parse the Keck api and return the current paricle count
+    """
     response = get(KECK_API)
     if response.ok:
         body = response.text.split('\n')
@@ -49,9 +57,13 @@ def particles():
     return 0
 
 def play_sound(path):
+    """
+    Use paplay to play the wav file at <path>
+    """
     system(f'paplay {path.absolute()}')
 
 
+# call the API and attempt to sound an alarm
 if args.simulate:
     p_3m = args.simulate
 else:
@@ -64,6 +76,8 @@ if p_3m > TOLERANCE:
 if not args.quiet:
     print(f'particle count: {p_3m}')
 
+# forever, sleep 5 minutes 
+# call the API and attempt to sound an alarm
 while True:
     sleep(5 * 60)  # sleep for 5 minutes
     p_3m = particles()
